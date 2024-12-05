2de044ff3972683334d91344420dcaa26a74d7dc
rlozl15 <eunjikim.dev@gmail.com>
2024-12-06 05:16:01 +0900

    feat: setting

---
+++ boardgame/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './App.css';
+import "./styles.css";
+import GameBoard from "./GameBoard";
+
+function App() {
+  return React.createElement(GameBoard);
+}
+
+export default App;
